491a00341d5541c8c36350adcf40b912ab1285e4
Your Name <you@example.com>
2020-12-28 05:36:50 +0900

    l1

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -4,7 +4,7 @@ function App() {
         <Container>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
-                    <h1>Todo App Test</h1>
+                    <h1>Todo App</h1>
                     <TodoListCard />
                 </Col>
             </Row>
